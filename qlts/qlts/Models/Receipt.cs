709ed2be307e5aee7ad5ea5d286405c345b7851a
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-04-19 18:14:55 +0900

    add

--- qlts/qlts/Models/Receipt.cs
+++ qlts/qlts/Models/Receipt.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,23 @@ namespace qlts.Models
 {
     public class Receipt : BaseEntity
     {
+        [StringLength(50)]
+        [Required]
+        [Index(IsUnique = true)]
+        public string Code { get; set; }
+        /// <summary>
+        /// Ngày bắt đầu sử dụng
+        /// </summary>
+        [Required]
+        public DateTime? ReceiptDate { get; set; }
+        /// <summary>
+        /// Ngày ghi tăng
+        /// </summary>
+        [Required]
+        public DateTime? IncreaseDate { get; set; }
+        /// <summary>
+        /// Tổng nguồn kinh phí
+        /// </summary>
+        public decimal TotalCost { get; set; }
     }
 }
