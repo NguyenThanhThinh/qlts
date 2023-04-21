ea84314bb19a351518322d58e828cbadd238a693
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-04-21 18:56:36 +0900

    add

---
+++ qlts/qlts/Controllers/BaseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
+using qlts.Datas;
+using qlts.Enums;
+using qlts.Models;
+
+namespace qlts.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected string UserId => User.Identity.GetUserId<string>();
+
+        protected void Alert( string message, bool isError = false )
+        {
+            if ( isError )
+                TempData["error"] = message;
+            else
+                TempData["success"] = message;
+        }
+
+        protected User GetCurrentUser( )
+        {
+            var identity = ( ClaimsPrincipal )Thread.CurrentPrincipal;
+            var value    = identity.Claims.Where ( c => c.Type == ClaimTypes.UserData ).Select ( c => c.Value ).SingleOrDefault();
+
+            return value == null ? new User() : JsonConvert.DeserializeObject<User> ( value );
+        }
+
+        protected void ShowError(Exception exception)
+        {
+
+            if (exception is BusinessException)
+            {
+                // Alert(exception.Message, true);
+                TempData["Exception"] = exception.Message;
+            }
+            else
+            {
+                // Alert(BusinessErrorMessages.BusinessError, true);
+                TempData["Exception"] = BusinessErrorMessages.BusinessError;
+            }
+        }
+
+        protected PositionType GetCurrentUserPosition()
+        {
+            var identity = (ClaimsPrincipal)Thread.CurrentPrincipal;
+            var value = identity.Claims.Where(c => c.Type == ClaimTypes.Role)
+                                .Select(c => c.Value).SingleOrDefault();
+
+            if (value == null) return PositionType.Warehouseman;
+
+            if ( value == PositionType.AccountingManager.ToString() )
+            {
+                return PositionType.AccountingManager;
+            }
+            else if ( value == PositionType.UnitManager.ToString() )
+            {
+                return PositionType.UnitManager;
+            }
+            else
+            {
+                return PositionType.Warehouseman;
+            }
+        }
+
+        protected JsonResponse GetResponse(bool success, string message = "", object data = null)
+        {
+            return new JsonResponse
+                   {
+                       Success = success,
+                       Message = message,
+                       Data = data
+                   };
+        }
+
+        protected string GenerateCode(string alias, int countCode)
+        {
+            string code = $"{alias}-{(++countCode):D4}";
+            return code;
+        }
+    }
+}
