af80bb2b1ef35bc078dc0637076f3efac498347d
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-04-21 16:28:19 +0900

    sdd

---
+++ qlts/qlts/Controllers/FeildsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qlts.Controllers
+{
+    public class FeildsController : Controller
+    {
+        // GET: Feilds
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUpdate()
+        {
+            return View();
+        }
+    }
+}
