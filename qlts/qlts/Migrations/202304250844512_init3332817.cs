ef3b1f314af0fd349377022219d5cf1d96959823
ntthinh <ntthinh@cusc.ctu.edu.vn>
2023-04-25 18:25:22 +0900

    up

---
+++ qlts/qlts/Migrations/202304250844512_init3332817.cs
@@ -0,0 +1,26 @@
+﻿namespace qlts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init3332817 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.FixedAssetManufacturers", "FixedAssetCode", c => c.String(nullable: false, maxLength: 100));
+            AddColumn("dbo.FixedAssetManufacturers", "FixedAssetName", c => c.String(nullable: false, maxLength: 200));
+            AddColumn("dbo.FixedAssetManufacturers", "Description", c => c.String());
+            AddColumn("dbo.FixedAssetManufacturers", "PartNumber", c => c.String());
+            AddColumn("dbo.FixedAssetManufacturers", "Unit", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.FixedAssetManufacturers", "Unit");
+            DropColumn("dbo.FixedAssetManufacturers", "PartNumber");
+            DropColumn("dbo.FixedAssetManufacturers", "Description");
+            DropColumn("dbo.FixedAssetManufacturers", "FixedAssetName");
+            DropColumn("dbo.FixedAssetManufacturers", "FixedAssetCode");
+        }
+    }
+}
---
+++ qlts/qlts/Models/FixedAssetManufacturer.cs
@@ -0,0 +1,35 @@
+﻿using qlts.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace qlts.Models
+{
+ 
+    public class FixedAssetManufacturer : BaseEntity
+    {
+        [StringLength(200)]
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime WarrantyPeriodDate { get; set; }
+
+
+        [StringLength(100)]
+        [Required]
+        public string FixedAssetCode { get; set; }
+
+        [StringLength(200)]
+        [Required]
+        public string FixedAssetName { get; set; }
+
+        public string Description { get; set; }
+
+        public string PartNumber { get; set; }
+
+        public FixedAssetUnit Unit { get; set; }
+    }
+}
